9a56c7f5c859bc689ea6103205363a7126a7aa03
Felipe Schottz <felipe.schottz@memora.com.br>
2024-05-03 17:38:06 -0300

    Add history

---
+++ kubernetes/history.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl create ns takehome --dry-run=client -o yaml > namespace.yaml
+kubectl create deployment go-app -n takehome --image=schottz/go_image --dry-run=client -o yaml > go-deployment.yaml
+
+# Added Liveness probe, Readiness probe, Prestop hook and Init Container following the Kubernetes Documentation
+
+kubectl -n takehome expose deployment/go-app --name=go-service --port 8080 --dry-run=client -o yaml > service.yaml
